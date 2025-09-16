3994164a3864f3dd680ad4ec481b2de1dc8892d5
Ashley2n <a.windbreacker@gmail.com>
2025-09-16 09:53:51 -0500

    Adding the folder Structure of the Budgeting app

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__, template_folder='Web/templates')
+app.secret_key = "BudgetBuddy00"
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
